117383bde5646d85af160fbb2ae052a48f58e2ac
root <root@DESKTOP-KD68TM7>
2024-01-28 23:28:19 -0300

    Commit inicial

---
+++ node_app/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'people'
+};
+const mysql = require('mysql2')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(nome) values('Vinicius')`
+connection.query(sql)
+connection.end()
+
+app.get('/', (req, res) => {
+    const connection = mysql.createConnection(config)
+    let final_return = '<h1>Full Cycle Rocks!</h1>'
+    const sql2 = `SELECT * FROM people`
+    let people = []
+    connection.query(sql2, function (err, results, fields) {
+        people = results.map(item => item.nome)
+        final_return += '<ul>'
+        people.forEach(element => {
+            final_return += '<li>' + element + '</li>'
+        });
+        final_return += '</ul>'
+        res.send(final_return)
+    })
+    connection.end()
+})
+
+app.listen(port, () => {
+    console.log('Rodando na porta ' + port)
+})
